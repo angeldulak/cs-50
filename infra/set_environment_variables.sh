0d8591bd2d25b8602f1695a8764eff58fde2c160
Christopher Keim <christopher.jl.keim@gmail.com>
2023-10-22 14:15:03 -0400

    Environment variable setup for C compiler.

---
+++ infra/set_environment_variables.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# Sets environment variables for C development.
+
+export CC="clang"
+export CFLAGS="-ferror-limit=1 -gdwarf-4 -ggdb3 -O0 -std=c11 -Wall -Werror -Wextra -Wno-gnu-folding-constant -Wno-sign-compare -Wno-unused-parameter -Wno-unused-variable -Wno-unused-but-set-variable -Wshadow"
+export LDLIBS="-lcrypt -lcs50 -lm"
