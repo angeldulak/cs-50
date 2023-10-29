2a129ac06d47dc600464d95c6419879bd7349186
angeldulak <thespacesbetween.media@gmail.com>
2023-10-29 14:34:15 -0400

    Took out -lcrypt for the moment.

--- infra/set_environment_variables.sh
+++ infra/set_environment_variables.sh
@@ -4,4 +4,7 @@
 
 export CC="clang"
 export CFLAGS="-ferror-limit=1 -gdwarf-4 -ggdb3 -O0 -std=c11 -Wall -Werror -Wextra -Wno-gnu-folding-constant -Wno-sign-compare -Wno-unused-parameter -Wno-unused-variable -Wno-unused-but-set-variable -Wshadow"
-export LDLIBS="-lcrypt -lcs50 -lm"
+export LDLIBS="-lm -lcs50"
+
+# Add these to LDLIBS when you need them
+# -lcrypt
